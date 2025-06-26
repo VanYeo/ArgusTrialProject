f499c3dfcb0be6eb80ac92d3605e9a2d750c79bb
TPSSOFT\Vanessa.yeo <vanessa.yeo@tpssoft.com>
2025-06-26 09:09:07 +0700

    update jwt token expiry time

--- backend/Repositories/TokenRepository.cs
+++ backend/Repositories/TokenRepository.cs
@@ -26,7 +26,9 @@ namespace backend.Repositories
                 configuration["Jwt:Issuer"],
                 configuration["Jwt:Audience"],
                 claims,
-                expires: DateTime.Now.AddMinutes(15),
+                expires: DateTime.Now.AddMinutes(
+                    int.TryParse(configuration["Jwt:ExpiryMinutes"], out var expiry) ? expiry : 60),
+
                 signingCredentials: credentials);
 
             return new JwtSecurityTokenHandler().WriteToken(token);
